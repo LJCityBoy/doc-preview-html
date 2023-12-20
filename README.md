773dd4208cabb21ae8446e913c1124631a6a2f35
S006284 <1334417517@qq.com>
2023-12-20 09:52:29 +0800

    优化版本

--- README.md
+++ README.md
@@ -4,12 +4,13 @@
 
 |     方法名      |      说明      |           类型          |  默认值 |  版本   |
 |:-------------:|:----------:|:---------------------:|:-------:|-------:|
-|  docFile2html  | docx文件转html | (file, header) => string     |    -    |  1.1.0+  |
+|  docFile2html  | docx文件转html | (file, bodyContainer, header) => string     |    -    |  1.1.0+  |
 ### docFile2html入参参数说明
 |     参数      |      说明      |    默认值  |  
 |:-------------:|:----------:|:---------------------:|
 |     file      |      文档路径、或者文档流      |    默认值  |
-|     header      |      当file为文档路径时，可以通过header参数设置请求头      |    默认值  |
+|     bodyContainer      |      展示文档的html容器      |    默认值  |
+|     header      |      可选参数，当file为文档路径时，可以通过header参数设置请求头      |    默认值  |
 
 
 ## Vue中示例
@@ -17,22 +18,23 @@
 ```
 <template>
   <div>
-    <div v-html="content"></div>
+    <div ref="content"></div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { docFile2html } from "doc-preview-html";
+import { ref, onMounted } from "vue";
+import { docFile2html } from "./components/DocPreview.js";
 
 const content = ref();
 
-// 调用方法，入参可以时文档地址，可以是二进制文档流
-docFile2html("./test1.docx").then((res) => {
-  content.value = res;
+onMounted(() => {
+  // 调用方法，入参可以是文档地址，可以是二进制文档流
+  docFile2html("./test.doc", content.value);
 });
 </script>
 
 <style scoped></style>
+
 
 ```
--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import * as docx from "docx-preview";
 
+const options = {
+  className: (string = "docx"), //class name/prefix for default and document style classes
+  inWrapper: (boolean = true), //enables rendering of wrapper around document content
+  ignoreWidth: (boolean = false), //disables rendering width of page
+  ignoreHeight: (boolean = false), //disables rendering height of page
+  ignoreFonts: (boolean = false), //disables fonts rendering
+  breakPages: (boolean = true), //enables page breaking on page breaks
+  ignoreLastRenderedPageBreak: (boolean = true), //disables page breaking on lastRenderedPageBreak elements
+  experimental: (boolean = false), //enables experimental features (tab stops calculation)
+  trimXmlDeclaration: (boolean = true), //if true, xml declaration will be removed from xml documents before parsing
+  useBase64URL: (boolean = false), //if true, images, fonts, etc. will be converted to base 64 URL, otherwise URL.createObjectURL is used
+  useMathMLPolyfill: (boolean = false), //@deprecated includes MathML polyfills for chrome, edge, etc.
+  renderChanges: false, //enables experimental rendering of document changes (inserions/deletions)
+  renderHeaders: true, //enables headers rendering
+  renderFooters: true, //enables footers rendering
+  renderFootnotes: true, //enables footnotes rendering
+  renderEndnotes: true, //enables endnotes rendering
+  debug: (boolean = false), //enables additional logging
+};
 /**
+ * 
  * doc文件转html
  * @param file 文件路径或者二进制文件流
- * @param {*} header 如果是文件路径，可能有需要配置请求头的需求
- * @returns
+ * @param {*} bodyContainer 展示文档的html容器
+ * @param {*} header 可选参数，如果是文件路径，可能有需要配置请求头的需求 
+ * @returns 
  */
-export const docFile2html = (file, header) => {
+export const docFile2html = (file, bodyContainer, header) => {
   return new Promise((res, rej) => {
     if (file instanceof ArrayBuffer || file instanceof Blob) {
-      const div = document.createElement("div");
       docx
-        .renderAsync(file, div)
-        .then(() => {
-          res(div.innerHTML);
+        .renderAsync(file, bodyContainer, "", options)
+        .then((obj) => {
+          res(obj);
         })
         .catch((error) => {
           console.log(error);
@@ -26,11 +46,10 @@ export const docFile2html = (file, header) => {
       xhr.send(JSON.stringify(header));
       xhr.onload = function () {
         if (xhr.status === 200 || xhr.status === 304) {
-          const div = document.createElement("div");
           docx
-            .renderAsync(xhr.response, div)
-            .then(() => {
-              res(div.innerHTML);
+            .renderAsync(xhr.response, bodyContainer, "", options)
+            .then((obj) => {
+              res(obj);
             })
             .catch((error) => {
               console.log(error);
